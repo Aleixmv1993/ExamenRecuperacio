7601de84199286a4ba2607f824b949936f32f21f
linux <linux@debian>
2017-02-13 18:51:26 +0100

    Examen recuperacio

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+	error_reporting(E_ALL);
+	ini_set('display_errors',1);
+
+	require 'tauler.php';
+
+	$tauler =  new Tauler(8,8,'dames');
+	
+	$tauler->show();
+
+	$tauler = new Tauler(10,10,'aleat');
+
+	$tauler->show();
+ ?>
---
+++ tauler.php
@@ -0,0 +1,90 @@
+<?php
+require 'quadre.php';
+
+class Tauler{
+	private $peces=array();
+	private $tipus;
+	private $nRows=null;
+	private $ncols=null;
+
+	function __construct($nRows=null, $nCols=null,$tipus){
+
+		$this->tipus=$tipus;
+		switch ($this->tipus) {
+			case 'dames': $this->dames($nRows=null,$nCols=null,$tipus);
+				break;
+			case 'aleat': $this->aleat($nRows=null,$nCols=null,$tipus);
+				break;
+		}
+	}
+	function dames($nRows=null, $nCols=null,$tipus){
+		//la intencio de aquesta funcio es que dins de peces es puguin guardar els diferents colors
+
+		for ($i=0 ; $i < $nRows; $i++ ) { 
+			for ($x=0; $x < $nCols ; $x++) { 
+
+				if ($i%2!=0) {
+					//aquest IF el que realitza es mirar si son parells en la files parelles per colocar el negre en les parelles i blanc en les imparelles
+					if($x%2 != 0){
+					$color = 'whitesmoke';
+					}else{
+					$color = 'black';
+					}
+
+				}else{
+					//Aquest if realitza tot el contrari al anterior
+					if($x%2 == 0){
+					$color = 'black';
+					}else{
+					$color = 'whitesmoke';
+					}
+				}
+				//guardar en el array quin color te cada vegada per despres poder mostrar amb el show.
+				$this->peces[i][x] = $color;
+			}
+		}
+		var_dump($this->peces);
+
+	}
+
+	function aleat($nRows=null, $nCols=null,$tipus){
+	
+	
+		for ($i=0 ; $i < $nRows; $i++ ) { 
+		
+			for ($x=0; $x < $nCols ; $x++) { 
+				//comporbo si el valor aleatori creat es per si es coloca el color blanc o el negre i el mateix amb el if inferior
+				$col= rand(1, $nCols);
+				if ($x%2==0) {
+					if($col%2 == 0){
+					$color = 'whitesmoke';
+					}else{
+					$color = 'black';
+					}
+
+				}else{
+					if($x%2 == 0){
+					$color = 'black';
+					}else{
+					$color = 'whitesmoke';
+					}
+				}
+				$this->peces[i][x] = $color;
+			}
+		}
+
+	}
+
+	function show(){
+		echo '<table border="1">';
+			for($i = 0; $i< $this->nRows; $i++){
+				echo '<tr>';
+					for($j = 0; $j < $nCols; $j++){
+						$quadre = new Quadre($this->peces[$i][$j]);
+					}
+				echo '</tr>';
+				}
+				echo '</table>';
+				}	
+
+}
